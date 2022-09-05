48f51673486abf9756bb9440a96afd74b3a06096
tatsutatsuguy <110403039+tatsutatsuguy@users.noreply.github.com>
2022-09-05 10:01:41 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('exampleModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
