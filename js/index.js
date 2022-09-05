5abdfca4e0799caea244a2804dbf74a3e70b8436
tatsutatsuguy <110403039+tatsutatsuguy@users.noreply.github.com>
2022-09-05 10:05:59 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+if (window.outerWidth <= 650){
+    document.getElementById("hero").innerHTML = "<img src='img/ot89_banner_reduced.png' class='img-fluid navbardrp' alt='OfficeTime89 Splash Image'>";
+  }else {
+    document.getElementById("hero").innerHTML = "<img src='img/ot89_banner_2560.png' class='img-fluid navbardrp' alt='OfficeTime89 Splash Image'>";
+}
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+function resize(){
+  if (window.outerWidth <= 650){
+      document.getElementById("hero").innerHTML = "<img src='img/ot89_banner_reduced.png' class='img-fluid navbardrp' alt='OfficeTime89 Splash Image'>";
+    }else {
+      document.getElementById("hero").innerHTML = "<img src='img/ot89_banner_2560.png' class='img-fluid navbardrp' alt='OfficeTime89 Splash Image'>";
+  }
+}
